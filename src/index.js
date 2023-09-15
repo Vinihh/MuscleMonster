526b3718e06bb141efea24e18dc96cfa01dff87f
Vinih <vinivsr2804@gmail.com>
2023-09-15 17:00:45 -0300

    s

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import Home from './pages/home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/home';
+import HomeAdm from './pages/adm/home-adm';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +13,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/adm/home' element={<HomeAdm />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
