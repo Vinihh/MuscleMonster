a85a621e688f1d5b3f6403eba0e6847b254e765d
Vinih <vinivsr2804@gmail.com>
2023-09-17 16:51:34 -0300

    qunado voltar termino

---
+++ src/pages/adm/produto-adm/index.js
@@ -0,0 +1,106 @@
+import { Link } from 'react-router-dom';
+import './index.scss';
+import InfoAdm from '../../../components/info-adm';
+import HeaderAdm from '../../../components/header-adm';
+import { useState } from 'react';
+import axios from 'axios';
+import { confirmAlert } from 'react-confirm-alert';
+
+export default function ProdutoAdm() {
+
+  const[busca,setBusca] = useState('');
+  const[listaProdutos,setListaProdutos] = useState([]);
+
+  async function buscarProduto() {
+    let resposta = await axios.get('http://localhost:5000/consultar-produto?busca=' + busca);
+    setListaProdutos(resposta.data);
+  }
+
+  async function removerProduto(id) {
+    
+    confirmAlert({
+      title: 'Produtos',
+      message: 'Tem certeza que deseja remover?',
+      buttons: [
+        {
+          label: 'Sim',
+          onClick: async () => {
+            let r = await axios.delete('http://localhost:5000/deletar-produto/' + id);
+            alert('Produto foi removido com sucesso');
+            buscarProduto();
+          }
+        },
+        {
+          label: 'Não'
+        }
+      ]
+    });
+    
+  }
+
+
+
+
+  return (
+
+    <div className="produto-adm">
+      <HeaderAdm />
+
+      <aside>
+        <InfoAdm />
+
+        <section className='faixa1-produto'>
+          <h1> Produtos</h1>
+
+          <div className='inp-busca'>
+            <input value={busca} onChange={e => setBusca(e.target.value)} placeholder='Procurar produtos...'></input>
+            <button onClick={buscarProduto}><img alt='' src='/assets/images/icon-busca2.png' /></button>
+          </div>
+          <nav>
+          <nav>
+          <table>
+              <colgroup>
+                <col style={{ width: 30 + '%' }} />
+                <col style={{ width: 15 + '%' }} />
+                <col style={{ width: 12 + '%' }} />
+                <col style={{ width: 12 + '%' }} />
+                <col style={{ width: 20 + '%' }} />
+              </colgroup>
+              <thead>
+                <tr>
+
+                  <th>Modelo</th>
+                  <th>Marca</th>
+                  <th>Ano</th>
+                  <th>Tipo</th>
+                  <th>placa</th>
+                </tr>
+              </thead>
+              <tbody>
+                {listaProdutos.map(item =>
+                  <tr>
+                    <td>{item.modelo}</td>
+                    <td>{item.marca}</td>
+                    <td>{item.ano}</td>
+                    <td>{item.tipo}</td>
+                    <td>{item.placa}</td>
+                    <td className='btns' style={{ display: 'flex', height: 20 }}>
+                      <i class="fa-regular fa-pen-to-square" ></i>
+                      <i class="fa-solid fa-delete-left" onClick={() => removerProduto(item.id)}></i>
+                    </td>
+                  </tr>  
+                )}
+                
+              </tbody>
+            </table>
+          </nav>
+          </nav>
+        </section>
+
+      </aside>
+
+    </div>
+  );
+}
+
+
---
+++ src/pages/adm/vendas-adm/index.js
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import './index.scss';
+import InfoAdm from '../../../components/info-adm';
+import HeaderAdm from '../../../components/header-adm';
+
+export default function VendasAdm() {
+  return (
+    <div className="vendas-adm">
+      <HeaderAdm />
+
+      <aside>
+        <InfoAdm />
+
+        <section className='faixa1-vendas '>
+          <h1> Vendas</h1>
+
+          <img src='/assets/images/img1-vendas.png' alt='' />
+
+          <button>Ir para a planilha</button>
+        </section>
+
+      </aside>
+
+    </div>
+  );
+}
+
+
