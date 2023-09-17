67f0384f304ddaa12fa60560c14708356726549d
Macys1704 <cherin1704@gmail.com>
2023-09-17 17:09:48 -0300

    addproduto com api pronta

--- src/pages/add-produto/index.js
+++ src/pages/add-produto/index.js
@@ -1,8 +1,39 @@
 import './index.scss';
 import HeaderAdm from '../../components/header-adm';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function HomeAdm() {
+
+  const [ nomeproduto, setNomeproduto ] = useState('')
+  const [ categoria, setCategoria ] = useState('')
+  const [ valor, setValor ] = useState('')
+  const [ estoque, setEstoque ] = useState('')
+
+  const [ salvo, setSalvo ] = useState('')
+
+
+  async function Inscrever() {
+    try {
+      const addproduto = {
+
+        nome: nomeproduto,
+        categoria: categoria,
+        valor: valor,
+        estoque: estoque
+      };
+
+      const url = 'http://localhost:5000/inserir'
+      const resposta = await axios.post(url, addproduto)
+
+    } catch (err) {
+      setSalvo(err.response.data.erro)
+    }
+
+
+  }
+
   return (
     <div className="add-produto">
       <HeaderAdm />
@@ -29,17 +60,21 @@ export default function HomeAdm() {
 
         <div className='inputs'>
           <h1>Nome</h1>
-          <input type='text' onChange={e => (e.target.value)} />
+          <input type='text' onChange={e => setNomeproduto(e.target.value)} />
 
           <h1>Categoria</h1>
-          <input type='text' onChange={e => (e.target.value)} />
+          <input type='text' onChange={e => setCategoria(e.target.value)} />
 
           <h1>Preço</h1>
-          <input type='text' onChange={e => (e.target.value)} />
+          <input type='text' onChange={e => setValor(e.target.value)} />
 
           <h1>Estoque</h1>
-          <input type='text' onChange={e => (e.target.value)} />
+          <input type='text' onChange={e => setEstoque(e.target.value)} />
           </div>
+
+          <button onClick={Inscrever}>Adicionar Produto</button>
+
+          <h1> {salvo} </h1>
 
         </div>
 
