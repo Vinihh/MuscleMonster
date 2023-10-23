1e13ae40e5a294e9eda813772938b923c687eb67
Vinih <vinivsr2804@gmail.com>
2023-10-23 16:19:48 -0300

    em andamento

--- src/pages/add-produto/index.js
+++ src/pages/add-produto/index.js
@@ -25,6 +25,7 @@ export default function HomeAdm() {
         nome: nomeproduto,
         categoria: categoria,
         valor: valor,
+        descricao:descricao,
         estoque: estoque
       };
 
