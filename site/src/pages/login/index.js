cd45b295474a6f428be373fe13edab99617352fd
Vinih <vinivsr2804@gmail.com>
2023-11-16 21:58:48 -0300

    COMIT

--- site/src/pages/login/index.js
+++ site/src/pages/login/index.js
@@ -31,7 +31,7 @@ export default function Login() {
 
     try {
 
-      const response = await axios.post( 'http://localhost:5000/login', user);
+      const response = await axios.post(API_URL + '/login', user);
 
       setTimeout(() => {
         storage('usuario-logado', response.data)
