52fe04ed3a9a48f2a364f9e238ede59a49808ce5
Vinih <vinivsr2804@gmail.com>
2023-11-23 19:35:04 -0300

    comit

--- api/src/repository/enderecoRepository.js
+++ api/src/repository/enderecoRepository.js
@@ -3,12 +3,10 @@ import conexao from "./connection.js";
 
 export async function CadastroEndereco(cadastro) {
 
-    let comando = `insert into tb_endereco(ds_rua, ds_bairro, ds_cidade, nr_cep, nr_numero, ds_complemento,nm_contato, tel_contato, ds_referencia, id_cliente)
-    values (?, ?, ?, ?, ?, ?, ?, ?,?,?);`;
-    
+    let comando = `INSERT INTO tb_endereco(ds_rua, ds_bairro, ds_cidade, nr_cep, nr_numero, ds_complemento, nm_contato, tel_contato, ds_referencia, id_cliente)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`;
 
     const [resposta] = await conexao.query(comando, [
-
         cadastro.rua,
         cadastro.bairro,
         cadastro.cidade,
@@ -18,12 +16,31 @@ export async function CadastroEndereco(cadastro) {
         cadastro.contato,
         cadastro.telefone,
         cadastro.referencia,
-        cadastro.idCliente,
-        cadastro.id
+        cadastro.id_cliente
     ]);
 
-    cadastro.id = resposta.insertId
+    cadastro.id = resposta.insertId;
 
     return cadastro;
+}
+
+export async function listarEndereco(id){
+    let comando = `
+        select 
+        id_endereco     as id,
+        ds_rua          as rua,
+        ds_bairro       as bairro,
+        ds_cidade       as cidade,
+        nr_cep          as cep,
+        ds_complemento  as complemento,
+        nm_contato      as contato,
+        tel_contato     as telefone,
+        ds_referencia   as referencia
+        from tb_endereco
+        where id_cliente = ?
+        `;
+
+    const [resposta] = await conexao.query(comando,[id]);
+    return resposta
 
 }
